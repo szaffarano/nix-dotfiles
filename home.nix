1da778a7be473327590603c5870164ce945a9076
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2023-02-02 19:15:55 +0100

    i3 rust

--- home.nix
+++ home.nix
@@ -42,9 +42,6 @@
     mypy
     black
 
-    # c
-    gcc
-
     nodejs
 
     go
@@ -61,8 +58,11 @@
 
     # sway
     fontconfig
+    font-awesome
     (nerdfonts.override {fonts = ["FiraCode" "DroidSansMono" "JetBrainsMono"];})
     rofi
+    foot
+    clipman
 
     firefox
     chromium
@@ -116,4 +116,55 @@
     };
   };
   programs.nix-index.enable = true;
+
+  programs.i3status-rust = {
+    enable = true;
+    bars = {
+      default = {
+        blocks = [
+          {
+            block = "disk_space";
+            path = "/";
+            alias = "/";
+            info_type = "available";
+            unit = "GB";
+            interval = 60;
+            warning = 20.0;
+            alert = 10.0;
+          }
+          {
+            block = "memory";
+            display_type = "memory";
+            format_mem = "{mem_used_percents}";
+            format_swap = "{swap_used_percents}";
+          }
+          {
+            block = "cpu";
+            interval = 1;
+          }
+          {
+            block = "load";
+            interval = 1;
+            format = "{1m}";
+          }
+          {
+            block = "time";
+            interval = 60;
+            format = "%a %d/%m %R";
+          }
+        ];
+        settings = {
+          theme = {
+            name = "solarized-dark";
+            overrides = {
+              idle_bg = "#123456";
+              idle_fg = "#abcdef";
+            };
+          };
+        };
+        icons = "awesome5";
+        theme = "gruvbox-dark";
+      };
+    };
+  };
 }
