99b2dfce91ab21c6f8ba1d58dea8c49fcf03d344
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2023-04-29 16:38:43 +0200

    chore: Add catppuccin theme to tmux

--- modules/home-manager/tmux/default.nix
+++ modules/home-manager/tmux/default.nix
@@ -2,34 +2,46 @@ _:
 { config, lib, pkgs, ... }: {
   options.tmux.enable = lib.mkEnableOption "tmux";
 
-  config = lib.mkIf config.tmux.enable {
-    programs.tmux = {
-      enable = true;
-      prefix = "C-a";
-      escapeTime = 1;
-      baseIndex = 1;
-      keyMode = "vi";
+  config =
+    let
+      catppuccin = pkgs.fetchFromGitHub {
+        owner = "catppuccin";
+        repo = "tmux";
+        rev = "4e48b09";
+        sha256 = "sha256-bXEsxt4ozl3cAzV3ZyvbPsnmy0RAdpLxHwN82gvjLdU=";
+      };
+    in
+    lib.mkIf config.tmux.enable {
+      programs.tmux = {
+        enable = true;
+        prefix = "C-a";
+        escapeTime = 1;
+        baseIndex = 1;
+        keyMode = "vi";
 
-      plugins = with pkgs; [
-        tmuxPlugins.better-mouse-mode
-        tmuxPlugins.sensible
-        tmuxPlugins.power-theme
-        tmuxPlugins.prefix-highlight
-        tmuxPlugins.pain-control
-        tmuxPlugins.yank
-      ];
+        plugins = with pkgs; [
+          tmuxPlugins.better-mouse-mode
+          tmuxPlugins.sensible
+          tmuxPlugins.prefix-highlight
+          tmuxPlugins.pain-control
+          tmuxPlugins.yank
+        ];
 
-      extraConfig = ''
-        set-option -sa terminal-overrides ',xterm-255color:RGB'
-        set -g status-position top
+        extraConfig = ''
+          run-shell "${catppuccin}/catppuccin.tmux"
 
-        bind C-a send-prefix -2
-        bind C-c new-session
-        bind Enter copy-mode
+          set -g @catppuccin_flavour 'mocha'
 
-        bind C-h previous-window
-        bind C-l next-window
-      '';
+          set-option -sa terminal-features ',xterm-kitty:RGB'
+          set -g status-position top
+
+          bind C-a send-prefix -2
+          bind C-c new-session
+          bind Enter copy-mode
+
+          bind C-h previous-window
+          bind C-l next-window
+        '';
+      };
     };
-  };
 }
