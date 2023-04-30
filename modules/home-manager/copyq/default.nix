eb81f551084dc89248a00d9475cb68672fe42bf0
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2023-04-30 19:24:46 +0200

    chore/misc configs (#49)
    * chore: Bump pre-commit hooks
    
    * chore: Fix copyq config
    
    * chore: Start using goenv
    
    * chore: Bump flakes

--- modules/home-manager/copyq/default.nix
+++ modules/home-manager/copyq/default.nix
@@ -4,8 +4,8 @@
 
   config = lib.mkIf config.copyq.enable {
 
-    xdg.configFile."copyq/copyq.conf".source = config.lib.file.mkOutOfStoreSymlink ./config/copyq.conf;
-    xdg.configFile."copyq/copyq-commands.ini".source = config.lib.file.mkOutOfStoreSymlink ./config/copyq-commands.ini;
+    xdg.configFile."copyq/copyq.conf".source = ./config/copyq.conf;
+    xdg.configFile."copyq/copyq-commands.ini".source = ./config/copyq-commands.ini;
 
     home.packages = with pkgs; [ copyq ];
   };
