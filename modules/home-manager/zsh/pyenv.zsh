1c86d1c75526aff96d1f385d15191c6ecded33d5
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2023-04-30 07:40:48 +0200

    chore: Fix pyenv config (#48)

--- modules/home-manager/zsh/pyenv.zsh
+++ modules/home-manager/zsh/pyenv.zsh
@@ -1,6 +1,6 @@
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+command -v pyenv >/dev/null && eval "$(pyenv init -)"
 
-# Load pyenv-virtualenv automatically by adding
-eval "$(pyenv virtualenv-init -)"
+# avoid issues with nix g++ environment
+alias pyenv="PATH=/bin:/usr/bin:$PATH pyenv"
