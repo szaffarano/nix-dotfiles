97be720455df632f0d7482e7de3f541b79fbf30c
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2023-06-08 16:06:25 +0200

    chore: Improve scratchpad scripts

--- modules/home-manager/sway/scripts/org.sh
+++ modules/home-manager/sway/scripts/org.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
-RUNNING_ORG=$(swaymsg -t get_tree -r | jq '.nodes[].nodes[].floating_nodes[] | select(.app_id=="org-mode")')
+RETRIES=10
+APP_ID="org-mode"
 
-[ -z "$RUNNING_ORG" ] && wezterm start --class=org-mode zsh --login -c "vim +WikiIndex"
+function running() {
+  RET=$(swaymsg -t get_tree -r | jq ".nodes[].nodes[].floating_nodes[] | select(.app_id==\"$1\")")
+  echo -n "$RET"
+}
+
+[ -z "$(running $APP_ID)" ] && nohup wezterm start --class=org-mode zsh --login -c "vim +WikiIndex" > /dev/null 2>&1 &
+
+COUNT=0
+while [ -z "$(running $APP_ID)" ] && [ "$COUNT" -lt "$RETRIES" ]; do
+  COUNT=$((COUNT+1))
+  sleep .1
+done
+
+sway [app_id="$APP_ID"] scratchpad show
