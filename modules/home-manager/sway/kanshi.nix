0bdd33fec219ea67a1eaad2c1c30133a7972a17d
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2023-08-19 14:11:27 +0200

    chore: Configure displays

--- modules/home-manager/sway/kanshi.nix
+++ modules/home-manager/sway/kanshi.nix
@@ -31,7 +31,35 @@
               status = "disable";
             }
             {
+              criteria = "HDMI-A-1";
+              status = "enable";
+            }
+            {
               criteria = "DP-1";
+              status = "disable";
+            }
+          ];
+        };
+        docked-alt = {
+          outputs = [
+            {
+              criteria = "eDP-1";
+              status = "disable";
+            }
+            {
+              criteria = "DP-1";
+              status = "enable";
+            }
+          ];
+        };
+        docked-office = {
+          outputs = [
+            {
+              criteria = "eDP-1";
+              status = "disable";
+            }
+            {
+              criteria = "DP-2";
               status = "enable";
             }
           ];
