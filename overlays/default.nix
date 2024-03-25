4ceeef37f88d337fd242641bd4b021d978c47af0
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2024-03-25 13:12:25 +0100

    Use external wofi-power-menu flake (#110)

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,20 +1,5 @@
 { outputs, inputs }: {
 
-  # TODO: review this logic
-  # For every flake input, aliases 'pkgs.inputs.${flake}' to
-  # 'inputs.${flake}.packages.${pkgs.system}' or
-  # 'inputs.${flake}.legacyPackages.${pkgs.system}'
-  flake-inputs = final: _: {
-    inputs = builtins.mapAttrs
-      (_: flake:
-        let
-          legacyPackages = ((flake.legacyPackages or { }).${final.system} or { });
-          packages = ((flake.packages or { }).${final.system} or { });
-        in
-        if legacyPackages != { } then legacyPackages else packages)
-      inputs;
-  };
-
   additions = final: prev: import ../pkgs { pkgs = final; };
 
   neovim = inputs.neovim-nightly.overlay;
