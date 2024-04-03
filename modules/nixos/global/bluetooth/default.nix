bea38bf68720bab08c34d1958844fd33868b3198
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2024-04-03 13:40:34 +0200

    Configure bluetooth

--- modules/nixos/global/bluetooth/default.nix
+++ modules/nixos/global/bluetooth/default.nix
@@ -1,15 +1,25 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 let cfg = config.nixos.bluetooth;
 in {
   options.nixos.bluetooth.enable = lib.mkEnableOption "bluetooth";
 
   config = lib.mkIf cfg.enable {
     # more info https://wiki.archlinux.org/title/bluetooth_mouse
-    systemd.tmpfiles.rules = [
-      "w    /sys/kernel/debug/bluetooth/hci0/conn_latency          -    -    -    -   0"
-      "w    /sys/kernel/debug/bluetooth/hci0/conn_min_interval     -    -    -    -   6"
-      "w    /sys/kernel/debug/bluetooth/hci0/conn_max_interval     -    -    -    -   9"
-    ];
+    systemd.tmpfiles = {
+      settings = {
+        "100-bluetooth_latency" = {
+          "/sys/kernel/debug/bluetooth/hci0/conn_latency" = {
+            w = { argument = "0"; };
+          };
+          "/sys/kernel/debug/bluetooth/hci0/conn_min_interval" = {
+            w = { argument = "15"; };
+          };
+          "/sys/kernel/debug/bluetooth/hci0/conn_max_interval" = {
+            w = { argument = "30"; };
+          };
+        };
+      };
+    };
 
     services.blueman.enable = true;
 
@@ -17,10 +27,12 @@ in {
       bluetooth = {
         enable = true;
         powerOnBoot = true;
+        package = pkgs.bluez5-experimental;
         settings = {
           General = {
             FastConnectable = true;
             Experimental = true;
+            KernelExperimental = true;
             ControllerMode = "dual";
           };
           Policy = {
