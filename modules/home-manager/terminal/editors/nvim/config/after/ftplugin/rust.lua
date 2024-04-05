4397ccefa44e1a44b45adf845b84301cf8a2c08a
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2024-04-05 11:33:53 +0200

    Move rust LSP configs to rustaceanvim

--- modules/home-manager/terminal/editors/nvim/config/after/ftplugin/rust.lua
+++ modules/home-manager/terminal/editors/nvim/config/after/ftplugin/rust.lua
@@ -1,5 +1,4 @@
--- Thanks TJ DeVries for the following code:
--- vim.opt_local.formatoptions:remove 'o'
+vim.opt_local.formatoptions:remove 'o'
 
 local ok, dap = pcall(require, 'dap')
 if not ok then
@@ -11,8 +10,27 @@ local function run_debugger()
   if dap.session() then
     dap.continue()
   else
-    require('sebas.dap').select_rust_runnable()
+    vim.cmd.RustLsp 'debuggables'
   end
 end
 
 vim.keymap.set('n', '<F9>', run_debugger, { buffer = vim.api.nvim_get_current_buf(), desc = 'Debug: Start/Continue' })
+
+vim.keymap.set('n', '<leader>ca', function()
+  vim.cmd.RustLsp 'codeAction'
+end, { buffer = vim.api.nvim_get_current_buf(), desc = '[C]ode [A]ction' })
+
+vim.g.rustaceanvim = {
+  tools = {
+    code_actions = {
+      ui_select_fallback = false,
+    },
+  },
+  server = {
+    on_attach = function(_, _) end,
+    default_settings = {
+      ['rust-analyzer'] = {},
+    },
+  },
+  dap = {},
+}
