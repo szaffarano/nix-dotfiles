5fcddf5ff21c04c5037b108b5357b5ea6c3d6b21
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2024-04-22 11:46:10 -0300

    chore: Add nix-community cache

--- flake.nix
+++ flake.nix
@@ -1,8 +1,12 @@
 {
   description = "Sebas's home-manager configurations";
 
-  nixConfig.extra-substituters = [ "https://szaffarano.cachix.org" ];
+  nixConfig.extra-substituters = [
+    "https://nix-community.cachix.org"
+    "https://szaffarano.cachix.org"
+  ];
   nixConfig.extra-trusted-public-keys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "szaffarano.cachix.org-1:T4qYO8SxoCddCRetQDQFUDc+tuBZyL7HuGcisMj4wiM="
   ];
 
