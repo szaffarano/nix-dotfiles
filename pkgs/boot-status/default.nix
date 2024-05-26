9bdebc47d4b9d002ee6e1577a1451e68d0426810
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2024-05-26 19:03:03 -0300

    feat: Add helper script (#173)

---
+++ pkgs/boot-status/default.nix
@@ -0,0 +1,16 @@
+{ lib
+, writeShellApplication
+, coreutils
+,
+}:
+(writeShellApplication {
+  name = "boot-status";
+  runtimeInputs = [ coreutils ];
+  text = builtins.readFile ./boot-status.sh;
+})
+  // {
+  meta = with lib; {
+    licenses = licenses.mit;
+    platforms = platforms.all;
+  };
+}
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,13 +5,14 @@ let
   firefox-addons = import ./firefox-addons { inherit (pkgs) fetchurl stdenv lib; };
   callPackage = pkgs.lib.callPackageWith (pkgs // packages);
   packages = {
+    boot-status = callPackage ./boot-status { };
     configure-gtk = callPackage ./configure-gtk { };
-    lock-screen = callPackage ./lock-screen { };
     flameshot-grim = callPackage ./flameshot { };
-    toggle-sway-scratchpad = callPackage ./toggle-sway-scratchpad { };
-    toggle-hyprland-scratchpad = callPackage ./toggle-hyprland-scratchpad { };
-    wallpaper = callPackage ./wallpaper { };
+    lock-screen = callPackage ./lock-screen { };
     sync-lid = callPackage ./sync-lid { };
+    toggle-hyprland-scratchpad = callPackage ./toggle-hyprland-scratchpad { };
+    toggle-sway-scratchpad = callPackage ./toggle-sway-scratchpad { };
+    wallpaper = callPackage ./wallpaper { };
   };
 in
 packages // firefox-addons
