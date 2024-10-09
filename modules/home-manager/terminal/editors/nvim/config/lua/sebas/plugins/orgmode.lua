b144fdb3d8159b98e5f3058a8bc41e670705e78d
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2024-10-09 13:56:49 +0200

    feat(wip): Configure orgmode

--- modules/home-manager/terminal/editors/nvim/config/lua/sebas/plugins/orgmode.lua
+++ modules/home-manager/terminal/editors/nvim/config/lua/sebas/plugins/orgmode.lua
@@ -1,5 +1,57 @@
+local org_base_path = '~/Documetns/org.new'
+
 return {
   'nvim-orgmode/orgmode',
   event = 'VeryLazy',
+  ft = { 'org' },
   dependencies = { 'nvim-orgmode/org-bullets.nvim', 'nvim-treesitter/nvim-treesitter' },
+  config = function()
+    require('orgmode').setup {
+      org_agenda_files = org_base_path .. '/**/*',
+      org_agenda_skip_deadline_if_done = false,
+      org_agenda_skip_scheduled_if_done = false,
+
+      org_default_notes_file = org_base_path .. '/refile.org',
+      org_log_into_drawer = 'LOGBOOK',
+      org_tags_column = 100,
+
+      org_deadline_warning_days = 60,
+
+      org_todo_keywords = { 'TODO(t)', 'PROGRESS(p)', '|', 'DONE(d)', 'REJECTED(r)' },
+
+      win_border = 'single',
+      win_split_mode = 'auto',
+
+      mappings = {
+        org = {
+          org_timestamp_up = '<A-k>',
+          org_timestamp_down = '<A-j>',
+        },
+      },
+
+      org_capture_templates = {
+        t = {
+          description = 'Refile',
+          template = '* TODO %?  :triage: \n  SCHEDULED: %T',
+        },
+        T = {
+          description = 'Todo',
+          template = '* TODO %? :personal:\n  SCHEDULED: %T',
+          target = org_base_path .. '/todos.org',
+          headline = 'Inbox',
+        },
+        w = {
+          description = 'Work todo',
+          template = '* TODO %?  :work: \n  SCHEDULED: %T',
+          target = org_base_path .. '/work.org',
+          headline = 'Inbox',
+        },
+        d = {
+          description = 'Daily',
+          template = '* Daily %U  :note: \n  %?',
+          target = org_base_path .. '/daily.org',
+        },
+      },
+    }
+  end,
 }
