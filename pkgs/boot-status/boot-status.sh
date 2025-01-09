41ccc81b336cf2976ab14e384c4ef393f1e6dc8d
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2025-01-09 05:49:35 -0300

    chore: Update boot-status helper script (#305)
    * chore: Update boot-status helper script
    
    * simplify output

--- pkgs/boot-status/boot-status.sh
+++ pkgs/boot-status/boot-status.sh
@@ -4,10 +4,7 @@ bootstrap="$(readlink -f /run/booted-system/kernel | cut -c 45- | sed s'/\/.*//g
 current="$(readlink -f /run/current-system/kernel | cut -c 45- | sed s'/\/.*//g')"
 restart=$([ "$bootstrap" = "$current" ] && echo false || echo true)
 
-printf '%15s %s\n%15s %s\n%15s %s\n' \
-	"Booted kernel:" \
-	"$bootstrap" \
-	"Current kernel:" \
-	"$current" \
-	"Restart:" \
-	"$restart"
+echo -e "
+Booted kernel:\t$bootstrap
+Current kernel:\t$current
+Restart:\t$restart" | column -t -Cright -Cleft -s$'\t' -o' '
