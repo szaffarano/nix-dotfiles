be46735b9f1296ce58b6235212fd939ae3cb6131
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2025-01-09 09:11:08 +0100

    simplify output

--- pkgs/boot-status/boot-status.sh
+++ pkgs/boot-status/boot-status.sh
@@ -4,10 +4,7 @@ bootstrap="$(readlink -f /run/booted-system/kernel | cut -c 45- | sed s'/\/.*//g
 current="$(readlink -f /run/current-system/kernel | cut -c 45- | sed s'/\/.*//g')"
 restart=$([ "$bootstrap" = "$current" ] && echo false || echo true)
 
-printf '%s\t%s\n%s\t%s\n%s\t%s\n' \
-	"Booted kernel:" \
-	"$bootstrap" \
-	"Current kernel:" \
-	"$current" \
-	"Restart:" \
-	"$restart" | column -t -Cright -Cleft -s$'\t' -o' '
+echo -e "
+Booted kernel:\t$bootstrap
+Current kernel:\t$current
+Restart:\t$restart" | column -t -Cright -Cleft -s$'\t' -o' '
