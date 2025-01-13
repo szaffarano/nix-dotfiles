aa57eebf46b4cc698f2993985c5d5e8c28426d7d
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2025-01-13 10:44:52 +0100

    chore: Configure nvim yaml formatter

--- modules/home-manager/terminal/editors/nvim/config/lua/sebas/plugins/format.lua
+++ modules/home-manager/terminal/editors/nvim/config/lua/sebas/plugins/format.lua
@@ -25,6 +25,7 @@ return {
       bash = { 'shfmt' },
       rust = { 'rustfmt' },
       toml = { 'taplo' },
+      yaml = { 'yq' },
       terraform = { 'terraform_fmt' },
     },
     format_on_save = function(bufnr)
