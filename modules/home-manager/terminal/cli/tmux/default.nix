1e3d4d82d5a839b8c268255bc13e467538ccd835
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2025-01-20 19:12:59 +0100

    chore: Configure neovim autosave

--- modules/home-manager/terminal/cli/tmux/default.nix
+++ modules/home-manager/terminal/cli/tmux/default.nix
@@ -52,6 +52,7 @@ in
           set -g default-terminal ''${TERM}
           set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'  # undercurl support
           set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'
+          set -g focus-events on
 
           # ============================================= #
           # Bindings                                      #
--- modules/home-manager/terminal/editors/nvim/config/lua/sebas/plugins/autosave.lua
+++ modules/home-manager/terminal/editors/nvim/config/lua/sebas/plugins/autosave.lua
@@ -1,15 +1,31 @@
 return {
   'okuuva/auto-save.nvim',
-  config = function()
-    require('auto-save').setup {
-      trigger_events = {
-        immediate_save = {},
-        defer_save = { 'FocusLost' },
-        cancel_deferred_save = {},
-      },
-      write_all_buffers = true,
-      debounce_delay = 500,
-      noautocmd = true,
-    }
-  end,
+  event = { 'FocusLost' },
+  opts = {
+    write_all_buffers = true,
+    debounce_delay = 500,
+    noautocmd = true,
+    trigger_events = {
+      immediate_save = { 'FocusLost' },
+      defer_save = {},
+      cancel_deferred_save = {},
+    },
+  },
+  config = {
+    group = vim.api.nvim_create_augroup('autosave', {}),
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'AutoSaveWritePost',
+      group = group,
+
+      callback = function(opts)
+        if opts.data.saved_buffer ~= nil then
+          local filename = vim.api.nvim_buf_get_name(opts.data.saved_buffer)
+          local max_levels = 3
+          filename = table.concat(vim.split(filename, '/'), '/', math.max(#vim.split(filename, '/') - max_levels + 1, 1))
+          vim.notify('AutoSave: saved ' .. filename .. ' at ' .. vim.fn.strftime '%H:%M:%S', vim.log.levels.INFO)
+        end
+      end,
+    }),
+  },
 }
