b2818e2c6da6a5c68dd40069b36dc5d6a71731f1
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2025-03-30 12:07:02 +0200

    chore: Bump wpaperd

--- overlays/wpaperd/default.nix
+++ overlays/wpaperd/default.nix
@@ -1,18 +1,22 @@
-# https://github.com/danyspin97/wpaperd/issues/79
-_final: prev: {
-  wpaperd = prev.wpaperd.overrideAttrs (old: rec {
-    version = "1.0.2-dev";
+# https://github.com/danyspin97/wpaperd/issues/115
+final: prev: {
+  wpaperd = prev.mpd-discord-rpc.overrideAttrs (_oldAttrs: rec {
+    pname = "wpaperd";
+    version = "1.1.2-dev";
     src = prev.fetchFromGitHub {
       owner = "danyspin97";
       repo = "wpaperd";
-      rev = "459c4e9c8bdd0f8b0572751efc96b59a2dd4cc78";
-      hash = "sha256-Jd/+JMMn1lgm1Oe78du6DVkFWGCWvbNuSXsIKzcCeME=";
+      rev = "3d7e6b0fabea249463d3b6fff5307a111e15bbdc";
+      hash = "sha256-YwWJQW809CawlIR/w+xVNxce6g4eQWi/9TG0gORipdE=";
     };
-    cargoDeps = old.cargoDeps.overrideAttrs (
-      prev.lib.const {
-        inherit src;
-        outputHash = "sha256-GEhD15shAGyLZXUndoaUTz8d5mNpOHapQJTP3RZ0dqM=";
-      }
-    );
+    buildInputs = [
+      prev.wayland
+      prev.libGL
+      prev.libxkbcommon
+    ];
+    cargoDeps = final.rustPlatform.fetchCargoVendor {
+      inherit src;
+      hash = "sha256-PZB3Dcd7iCmWZBqEmIrUlNy1Gf95YVEbgd1RPFSdNxU=";
+    };
   });
 }
