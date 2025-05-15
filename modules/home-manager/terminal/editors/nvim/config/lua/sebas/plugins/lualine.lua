e3eb9adfe94af86963a8d1fa5cc8d20daa1ba3d4
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2025-05-15 03:57:58 -0300

    chore(nvim): Configure plugins (#343)
    * chore(nvim): Configure lualine
    
    * chore(nvim): Configure ufo plugin

--- modules/home-manager/terminal/editors/nvim/config/lua/sebas/plugins/lualine.lua
+++ modules/home-manager/terminal/editors/nvim/config/lua/sebas/plugins/lualine.lua
@@ -1,5 +1,11 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {},
+  opts = {
+    options = {
+      component_separators = '',
+      section_separators = '',
+      theme = 'auto',
+    },
+  },
 }
