f82fd27ded63d68f97fd427db3146e4cdde56900
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2025-06-05 11:42:43 +0200

    feat(wip): VSCode tests

--- users/szaffarano/zaffarano-elastic.nix
+++ users/szaffarano/zaffarano-elastic.nix
@@ -2,19 +2,48 @@
   home = {
     custom = {
       features.enable = [];
-      allowed-unfree-packages = with pkgs; [windsurf];
+      allowed-unfree-packages = with pkgs; [
+        vscode
+        vscode-extensions.github.copilot
+        vscode-extensions.github.copilot-chat
+        vscode-extensions.ms-vscode-remote.vscode-remote-extensionpack
+        vscode-extensions.ms-vscode-remote.remote-containers
+        vscode-with-extensions
+        windsurf
+      ];
       permitted-insecure-packages = ["python-2.7.18.8"]; # needed by bazel-5.1.1
     };
-    packages = [
-      pkgs.bazel_5_1_1
-      pkgs.windsurf
-      pkgs.pkg-config
-      pkgs.openssl
-      pkgs.asciinema
-      pkgs.asciinema-agg
-      pkgs.upx
-      pkgs.pkgsCross.mingwW64.stdenv.cc
-      pkgs.pkgsCross.mingwW64.windows.mingw_w64_pthreads
+    packages = with pkgs; [
+      asciinema
+      asciinema-agg
+      bazel_5_1_1
+      openssl
+      pkg-config
+      pkgsCross.mingwW64.stdenv.cc
+      pkgsCross.mingwW64.windows.mingw_w64_pthreads
+      upx
+      (
+        vscode-with-extensions.override {
+          # When the extension is already available in the default extensions set.
+          vscodeExtensions = with vscode-extensions;
+            [
+              asvetliakov.vscode-neovim
+              enkia.tokyo-night
+              github.copilot
+              github.copilot-chat
+              ms-pyright.pyright
+              ms-python.python
+              ms-vscode-remote.vscode-remote-extensionpack
+              ms-vscode-remote.remote-containers
+              rust-lang.rust-analyzer
+              vscode-icons-team.vscode-icons
+            ]
+            # Concise version from the vscode market place when not available in the default set.
+            ++ vscode-utils.extensionsFromVscodeMarketplace [
+            ];
+        }
+      )
+      windsurf
     ];
   };
 
