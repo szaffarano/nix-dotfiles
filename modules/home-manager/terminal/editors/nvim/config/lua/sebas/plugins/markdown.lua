6e18dfb9bd7f4d2456a117bf3dea15d12d3f3bea
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2025-06-13 12:01:29 +0200

    fix(nvim): update markdown and spell settings
    - Removed duplicate and redundant filetypes from ltex_extra plugin
    - Added 'mdx' to autocmd FileType list for spell checking
    - Extended spelllang to include German and Swedish languages
    - Minor dependency formatting fix in markdown plugin config

--- modules/home-manager/terminal/editors/nvim/config/lua/sebas/plugins/markdown.lua
+++ modules/home-manager/terminal/editors/nvim/config/lua/sebas/plugins/markdown.lua
@@ -5,7 +5,7 @@ vim.treesitter.language.register('markdown', 'mdx')
 
 return {
   'MeanderingProgrammer/render-markdown.nvim',
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
   opts = {
