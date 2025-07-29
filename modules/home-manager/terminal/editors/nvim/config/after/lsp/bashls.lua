56af9bcde7009d7c028a86c9b16b4784d777f297
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2025-07-29 11:48:53 +0200

    feat(nvim): Configure bashls lsp server

--- modules/home-manager/terminal/editors/nvim/config/after/lsp/bashls.lua
+++ modules/home-manager/terminal/editors/nvim/config/after/lsp/bashls.lua
@@ -1,4 +1,12 @@
 return {
+  cmd = {
+    'mise',
+    'exec',
+    'node@latest',
+    '--',
+    'bash-language-server',
+    'start',
+  },
   settings = {
     filetypes = { 'sh', 'zsh', 'bash' },
   },
