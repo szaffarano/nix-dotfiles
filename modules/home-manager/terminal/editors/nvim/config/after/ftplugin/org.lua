86cb225b9a1fbc6abc6322e996c83867e94cd028
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2025-09-01 08:31:49 +0200

    Update indent config

--- modules/home-manager/terminal/editors/nvim/config/after/ftplugin/org.lua
+++ modules/home-manager/terminal/editors/nvim/config/after/ftplugin/org.lua
@@ -2,7 +2,7 @@ vim.opt_local.concealcursor = 'nc'
 vim.opt_local.conceallevel = 2
 vim.opt_local.foldenable = true
 vim.opt_local.foldlevel = 99
-vim.opt_local.foldlevelstart = 1
+vim.opt_local.foldlevelstart = 2
 
 vim.keymap.set('n', '<localleader>oti', function()
   Org:indent_mode()
--- modules/home-manager/terminal/editors/nvim/config/lua/sebas/plugins/ufo.lua
+++ modules/home-manager/terminal/editors/nvim/config/lua/sebas/plugins/ufo.lua
@@ -121,6 +121,7 @@ return {
   enabled = true,
   init = function()
     vim.o.foldcolumn = 'auto'
+    vim.o.foldlevel = 99
     vim.o.foldlevelstart = 99
     vim.o.foldenable = true
     vim.o.fillchars = 'eob: ,fold: ,foldopen:,foldsep: ,foldclose:'
