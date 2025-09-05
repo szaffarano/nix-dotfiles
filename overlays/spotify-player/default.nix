ffb912a8ff74dba41fa6ec5b9d476957de0f42e8
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2025-09-05 07:46:30 +0200

    chore(spotify-player): Patch build

--- overlays/spotify-player/default.nix
+++ overlays/spotify-player/default.nix
@@ -8,6 +8,13 @@ final: prev: {
       rev = "v${version}";
       hash = "sha256-nOswrYt9NrzJV6CFBWZCpj/wIJnIgmr3i2TreAKGGPI=";
     };
+    patches = [
+      (prev.fetchpatch {
+        name = "fix-build-failure.patch";
+        url = "https://github.com/aome510/spotify-player/commit/77af13b48b2a03e61fef1cffea899929057551dc.patch";
+        hash = "sha256-5q8W0X49iZLYdwrBiZJTESb628VPamrm0zEYwDm8CVk=";
+      })
+    ];
     cargoDeps = final.rustPlatform.fetchCargoVendor {
       inherit src;
       hash = "sha256-YarKRApcQHom3AQIirqGdmUOuy5B+BRehLijvF/GRPc=";
