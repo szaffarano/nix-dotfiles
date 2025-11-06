dbfed01f7fe830dcab6d2e76e69b21bb3a15af1a
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2025-11-06 09:15:39 +0100

    Add rust keybindings

--- modules/home-manager/terminal/editors/nvim/config/after/ftplugin/rust.lua
+++ modules/home-manager/terminal/editors/nvim/config/after/ftplugin/rust.lua
@@ -4,27 +4,39 @@ local bufnr = vim.api.nvim_get_current_buf()
 
 vim.keymap.set('n', '<leader>ca', function()
   vim.cmd.RustLsp 'codeAction'
-end, { buffer = bufnr, desc = '[C]ode [A]ction (Rust)' })
+end, { buffer = bufnr, desc = '[C]ode [A]ction' })
 
 vim.keymap.set('n', '<leader>rd', function()
   vim.cmd.RustLsp 'relatedDiagnostics'
-end, { buffer = bufnr, desc = '[R]elated [D]iagnostics (Rust)' })
+end, { buffer = bufnr, desc = '[R]ust related [D]iagnostics' })
 
-vim.keymap.set('n', '<leader>h', function()
+vim.keymap.set('n', '<leader>rh', function()
   vim.cmd.RustLsp { 'hover', 'range' }
-end, { buffer = bufnr, desc = '[H]over range (Rust)' })
+end, { buffer = bufnr, desc = '[R]ust [H]over range' })
 
 vim.keymap.set('n', 'K', function()
   vim.cmd.RustLsp { 'hover', 'actions' }
 end, { buffer = bufnr, desc = 'Hover Documentation (Rust)' })
 
-vim.keymap.set('n', '<leader>e', function()
+vim.keymap.set('n', '<leader>re', function()
   vim.cmd.RustLsp { 'renderDiagnostic', 'current' }
-end, { buffer = bufnr, desc = 'Show diagnostic [E]rror messages (Rust)' })
+end, { buffer = bufnr, desc = '[R]ust show diagnostic [E]rror messages' })
 
-vim.keymap.set('n', '<leader>E', function()
+vim.keymap.set('n', '<leader>rE', function()
   vim.cmd.RustLsp { 'explainError', 'current' }
-end, { buffer = bufnr, desc = 'Explain [E]rror (Rust)' })
+end, { buffer = bufnr, desc = '[R]ust explain [E]rror' })
+
+vim.keymap.set('n', '<leader>rod', function()
+  vim.cmd.RustLsp 'openDocs'
+end, { buffer = bufnr, desc = '[R]ust [O]pen [D]ocs' })
+
+vim.keymap.set('n', '<leader>rc', function()
+  vim.cmd.RustLsp 'openCargo'
+end, { buffer = bufnr, desc = '[R]ust open [C]argo.toml' })
+
+vim.keymap.set('n', '<leader>rj', function()
+  vim.cmd.RustLsp 'joinLines'
+end, { buffer = bufnr, desc = '[R]ust [J]oin lines' })
 
 local is_dap_installed, dap = pcall(require, 'dap')
 if is_dap_installed then
@@ -40,8 +52,3 @@ if is_dap_installed then
 else
   print 'dap not found'
 end
-
--- mappings to review:
--- vim.cmd.RustLsp('openCargo')
--- vim.cmd.RustLsp('openDocs')
--- vim.cmd.RustLsp('joinLines'):qa
