cdda52f2666097605b14dd2c431f4fe42a14bc03
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2025-11-13 17:46:45 +0100

    feat: Enhance Neovim treesitter and update desktop packages
    - Add comprehensive treesitter-textobjects keybindings for code navigation
    - Fix treesitter parser path configuration to use site/parser
    - Simplify init.lua by removing VSCode-specific logic
    - Add hyprlang filetype detection via plugin file
    - Update desktop packages: re-enable Anki, switch from foliate/koodo-reader to readest
    - Update rust10x-vscode theme to latest version
    - Enable calibre feature on zaffarano-elastic system

--- modules/home-manager/terminal/editors/nvim/config/init.lua
+++ modules/home-manager/terminal/editors/nvim/config/init.lua
@@ -5,44 +5,13 @@ local is_vscode = vim.g.vscode ~= nil
 vim.g.base16_colorspace = 256
 vim.g.base16_background_transparent = 1
 
-if not is_vscode then
-  local ok, lazy = pcall(require, 'lazy')
-  if ok then
-    lazy.setup('sebas.plugins', {
-      change_detection = {
-        notify = false,
-      },
-    })
-  else
-    print 'lazy.nvim not found'
-  end
+local ok, lazy = pcall(require, 'lazy')
+if ok then
+  lazy.setup('sebas.plugins', {
+    change_detection = {
+      notify = false,
+    },
+  })
 else
-  -- still a WIP, review which plugins setup
-  local vscode = require 'vscode'
-
-  local function map(mode, lhs, rhs)
-    vim.keymap.set(mode, lhs, function()
-      vscode.call(rhs)
-    end, { silent = true, noremap = true })
-  end
-
-  -- Remap folding keys
-  map('n', 'zM', 'editor.foldAll')
-  map('n', 'zR', 'editor.unfoldAll')
-  map('n', 'zc', 'editor.fold')
-  map('n', 'zC', 'editor.foldRecursively')
-  map('n', 'zo', 'editor.unfold')
-  map('n', 'zO', 'editor.unfoldRecursively')
-  map('n', 'za', 'editor.toggleFold')
+  print 'lazy.nvim not found'
 end
-
--- Update `runtimepath` with treesitter parsers configured by nix config
--- It has to be located here because `lazy` overrides `runtimepath`
-local treesitter_parsers = vim.fn.stdpath 'data' .. '/treesitter-parsers'
-vim.opt.runtimepath:append(treesitter_parsers)
-
-vim.filetype.add {
-  pattern = {
-    ['.*/hypr/.*%.conf'] = 'hyprlang',
-  },
-}
--- modules/home-manager/terminal/editors/nvim/config/lua/sebas/plugins/treesitter.lua
+++ modules/home-manager/terminal/editors/nvim/config/lua/sebas/plugins/treesitter.lua
@@ -1,15 +1,136 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-  },
-  build = ':TSUpdate',
-  opts = {
-    auto_install = false,
-    ensure_installed = {},
-    highlight = {
-      enable = true,
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    opts = {
+      auto_install = false,
+      ensure_installed = {},
+      highlight = {
+        enable = true,
+      },
+      indent = { enable = true },
     },
-    indent = { enable = true },
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    branch = 'main',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('nvim-treesitter-textobjects').setup {
+        select = {
+          lookahead = true,
+        },
+        move = {
+          set_jumps = true,
+        },
+      }
+
+      local select = require 'nvim-treesitter-textobjects.select'
+      local move = require 'nvim-treesitter-textobjects.move'
+
+      vim.keymap.set({ 'x', 'o' }, 'af', function()
+        select.select_textobject('@function.outer', 'textobjects')
+      end, { desc = 'Select [A]round [F]unction' })
+      vim.keymap.set({ 'x', 'o' }, 'if', function()
+        select.select_textobject('@function.inner', 'textobjects')
+      end, { desc = 'Select [I]nside [F]unction' })
+
+      vim.keymap.set({ 'x', 'o' }, 'ac', function()
+        select.select_textobject('@class.outer', 'textobjects')
+      end, { desc = 'Select [A]round [C]lass' })
+      vim.keymap.set({ 'x', 'o' }, 'ic', function()
+        select.select_textobject('@class.inner', 'textobjects')
+      end, { desc = 'Select [I]nside [C]lass' })
+
+      vim.keymap.set({ 'x', 'o' }, 'ap', function()
+        select.select_textobject('@parameter.outer', 'textobjects')
+      end, { desc = 'Select [A]round [P]arameter' })
+      vim.keymap.set({ 'x', 'o' }, 'ip', function()
+        select.select_textobject('@parameter.inner', 'textobjects')
+      end, { desc = 'Select [I]nside [P]arameter' })
+
+      vim.keymap.set({ 'x', 'o' }, 'al', function()
+        select.select_textobject('@loop.outer', 'textobjects')
+      end, { desc = 'Select [A]round [L]oop' })
+      vim.keymap.set({ 'x', 'o' }, 'il', function()
+        select.select_textobject('@loop.inner', 'textobjects')
+      end, { desc = 'Select [I]nside [L]oop' })
+
+      vim.keymap.set({ 'x', 'o' }, 'ai', function()
+        select.select_textobject('@conditional.outer', 'textobjects')
+      end, { desc = 'Select [A]round [I]f conditional' })
+      vim.keymap.set({ 'x', 'o' }, 'ii', function()
+        select.select_textobject('@conditional.inner', 'textobjects')
+      end, { desc = 'Select [I]nside [I]f conditional' })
+
+      vim.keymap.set({ 'x', 'o' }, 'as', function()
+        select.select_textobject('@local.scope', 'locals')
+      end, { desc = 'Select [A]round [S]cope' })
+
+      vim.keymap.set({ 'x', 'o' }, 'ai', function()
+        select.select_textobject('@block.inner', 'textobjects')
+      end, { desc = 'Select [A]round [B]lock' })
+      vim.keymap.set({ 'x', 'o' }, 'ii', function()
+        select.select_textobject('@block.outer', 'textobjects')
+      end, { desc = 'Select [I]nside [B]block' })
+
+      vim.keymap.set({ 'n', 'x', 'o' }, ']f', function()
+        move.goto_next_start('@function.outer', 'textobjects')
+      end, { desc = 'Next [F]unction start' })
+      vim.keymap.set({ 'n', 'x', 'o' }, ']c', function()
+        move.goto_next_start('@class.outer', 'textobjects')
+      end, { desc = 'Next [C]lass start' })
+
+      vim.keymap.set({ 'n', 'x', 'o' }, ']o', function()
+        move.goto_next_start({ '@loop.inner', '@loop.outer' }, 'textobjects')
+      end, { desc = 'Next l[O]op start' })
+
+      vim.keymap.set({ 'n', 'x', 'o' }, ']s', function()
+        move.goto_next_start('@local.scope', 'locals')
+      end, { desc = 'Next [S]cope start' })
+      vim.keymap.set({ 'n', 'x', 'o' }, ']z', function()
+        move.goto_next_start('@fold', 'folds')
+      end, { desc = 'Next fold start' })
+
+      vim.keymap.set({ 'n', 'x', 'o' }, ']M', function()
+        move.goto_next_end('@function.outer', 'textobjects')
+      end, { desc = 'Next function end ([M])' })
+      vim.keymap.set({ 'n', 'x', 'o' }, ']C', function()
+        move.goto_next_end('@class.outer', 'textobjects')
+      end, { desc = 'Next [C]lass end' })
+
+      vim.keymap.set({ 'n', 'x', 'o' }, '[f', function()
+        move.goto_previous_start('@function.outer', 'textobjects')
+      end, { desc = 'Previous [F]unction start' })
+      vim.keymap.set({ 'n', 'x', 'o' }, '[c', function()
+        move.goto_previous_start('@class.outer', 'textobjects')
+      end, { desc = 'Previous [C]lass start' })
+      vim.keymap.set({ 'n', 'x', 'o' }, '[o', function()
+        move.goto_previous_start({ '@loop.inner', '@loop.outer' }, 'textobjects')
+      end, { desc = 'Previous l[O]op start' })
+      vim.keymap.set({ 'n', 'x', 'o' }, '[s', function()
+        move.goto_previous_start('@local.scope', 'locals')
+      end, { desc = 'Previous [S]cope start' })
+      vim.keymap.set({ 'n', 'x', 'o' }, '[z', function()
+        move.goto_previous_start('@fold', 'folds')
+      end, { desc = 'Previous fold start' })
+
+      vim.keymap.set({ 'n', 'x', 'o' }, '[M', function()
+        move.goto_previous_end('@function.outer', 'textobjects')
+      end, { desc = 'Previous function end ([M])' })
+      vim.keymap.set({ 'n', 'x', 'o' }, '[C', function()
+        move.goto_previous_end('@class.outer', 'textobjects')
+      end, { desc = 'Previous [C]lass end' })
+
+      vim.keymap.set({ 'n', 'x', 'o' }, ']d', function()
+        move.goto_next('@conditional.outer', 'textobjects')
+      end, { desc = 'Next con[D]itional' })
+      vim.keymap.set({ 'n', 'x', 'o' }, '[d', function()
+        move.goto_previous('@conditional.outer', 'textobjects')
+      end, { desc = 'Previous con[D]itional' })
+    end,
   },
 }
