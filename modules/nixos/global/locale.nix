11935c3c52be9b85c65a072a28e338da1b0c24fe
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2026-07-08 02:24:57 -0300

    Revert "config(locale): Change timezone to Buenos Aires (#504)"
    This reverts commit 4ff6f36c93b42489e3be6dac05c77e0487d369ea.

--- modules/nixos/global/locale.nix
+++ modules/nixos/global/locale.nix
@@ -12,7 +12,6 @@
         "sv_SE.UTF-8/UTF-8"
       ];
     };
-    time.timeZone = lib.mkDefault "America/Buenos_Aires";
-    # time.timeZone = lib.mkDefault "Europe/Stockholm";
+    time.timeZone = lib.mkDefault "Europe/Stockholm";
   };
 }
